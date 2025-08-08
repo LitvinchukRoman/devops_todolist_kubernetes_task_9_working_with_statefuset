18721ab5c058059e304b7074a08c9b2cfdc913e5
litvi <litvin4uk.roman@gmail.com>
2025-08-09 00:00:33 +0300

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,10 @@ set -e
 
 kind create cluster --config cluster.yml
 
-kubectl apply -f namespace.yml
-kubectl apply -f mysql-secret.yml
-kubectl apply -f mysql-initdb.yml
-kubectl apply -f statefulSet.yml
+kubectl apply -f .infrastructure/namespace.yml
+kubectl apply -f .infrastructure/mysql-secret.yml
+kubectl apply -f .infrastructure/mysql-initdb.yml
+kubectl apply -f .infrastructure/statefulSet.yml
 
-kubectl apply -f app-secret.yml
-kubectl apply -f deployment.yml
+kubectl apply -f .infrastructure/app-secret.yml
+kubectl apply -f .infrastructure/deployment.yml
