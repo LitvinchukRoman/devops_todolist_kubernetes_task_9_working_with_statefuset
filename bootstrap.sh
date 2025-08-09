51121448e0766eb5f3fb41e607439f6096174c1b
litvi <litvin4uk.roman@gmail.com>
2025-08-09 11:07:08 +0300

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ set -e
 kind create cluster --config cluster.yml
 
 kubectl apply -f .infrastructure/namespace.yml
+kubectl apply -f .infrastructure/namespaceMysql.yml
 kubectl apply -f .infrastructure/mysql-secret.yml
 kubectl apply -f .infrastructure/mysql-initdb.yml
 kubectl apply -f .infrastructure/statefulSet.yml
